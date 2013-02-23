663af4de116b347fe615e664d73e826c46f732f7
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-02-23 16:37:04 -0500

    add simple method to look up any user's location

--- octomap.rb
+++ octomap.rb
@@ -3,9 +3,14 @@ require 'octokit'
 require 'pp'
 require 'json'
 
-john = Octokit.user("JohnKellyFerguson")
+#Gets the location of any user passed in as a parameter:
+#ISSUES:
+#1. The input must be in the form of a string.
+#2. If the user has no location key, the following error is returned:
+#KeyError: key not found: "location"
 
-#location is a string
-location = john.fetch("location")
-
-puts location
+def user_loc(name)
+  id = Octokit.user(name)
+  location = id.fetch("location")
+  location
+end
