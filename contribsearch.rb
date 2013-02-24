fb3643401eda79d5bfd9a6b841bd2355cbf2a845
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-02-23 19:40:12 -0500

    Add initial working method to return all contributor usernames from a repo

--- contribsearch.rb
+++ contribsearch.rb
@@ -3,7 +3,9 @@ require 'octokit'
 require 'pp'
 require 'json'
 
-
-octo = Octokit.contribs('pengwynn/octokit')
-
-pp octo
+#returns an array of all usernames from a repo
+def repo_contrib_locs(user_repo)
+  repo_contribs = Octokit.contribs(user_repo)
+  contrib_locs = repo_contribs.collect { |user| user.fetch("login") }
+  contrib_locs
+end
