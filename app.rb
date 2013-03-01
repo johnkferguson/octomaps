242f8b88e400dd997a302913e623acd8570e9ed0
Masha Rikhter <masha.rikhter@gmail.com>
2013-03-01 18:21:18 -0500

    analytics

--- app.rb
+++ app.rb
@@ -10,31 +10,23 @@ end
 
 get '/map' do
   @repo = Repo.new(params[:owner], params[:repo])
-  
   begin
     @repo.locations
   rescue
     redirect '/notfound'
   end
-  
   data_table_markers = GoogleVisualr::DataTable.new
   data_table_markers.new_column('string' , 'Location' )
   data_table_markers.new_column('number' , 'Contributions')
   data_table_markers.add_rows(@repo.location_count.size)
   i = 0
-
   @repo.location_count.each do |location, count|
     data_table_markers.set_cell(i,0,location)
     data_table_markers.set_cell(i,1, count)
     i += 1
   end
-  
-  # while i < @repo.location_count.count
-  # end
-  
   opts = { :displayMode => 'markers', :region => 'world', :legend => 'none', 
            :colors => ['FF8F86', 'C43512']}
-  
   @chart_markers = GoogleVisualr::Interactive::GeoChart.new(data_table_markers, opts)
   
   erb :map
