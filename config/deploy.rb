e505d695983b1b275ab010c77b4e8cdf739bd7e0
John Kelly Ferguson <hello@johnkellyferguson.com>
2013-09-07 01:01:19 -0400

    Tweaking deploy settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}" 
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}" 
     run "mkdir -p #{shared_path}/config" 
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml" 
+    put File.read("config/database.example.rb"), "#{shared_path}/config/database.rb" 
     puts "Now edit the config files in #{shared_path}." 
   end 
 
