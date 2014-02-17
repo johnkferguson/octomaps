9e1409c207e341ca2f41c3557865844e162cedc9
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-02-17 00:44:24 -0500

    Refactor location spec

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,41 +1,21 @@
 require 'spec_helper'
 
 describe Location do
-  before(:each) do
-    @location = Location.new(name: "New York, NY")
-  end
-
-  subject { @location }
+  let(:location) { Location.new(name: "New York, NY") }
 
   it { should respond_to(:name) }
   it { should belong_to(:city) }
   it { should have_many(:users) }
-
-  describe "when name is not present" do
-    before { @location.name = ""}
-    it { should_not be_valid }
-  end
-
-  describe "when name is present" do
-    before { @location.name = "test" }
-    it { should be_valid }
-  end
-
-  describe "when name is already taken" do
-    before do
-      location_with_same_name = @location.dup
-      location_with_same_name.name = @location.name
-      location_with_same_name.save
-    end
-    it { should_not be_valid }
-  end
+  it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
 
   describe "when the name is entered with capital letters" do
     before do
-      @location.save
+      Location.any_instance.stub(:geocode_location).and_return(nil)
+      location.save
     end
     it "should save as lowercase" do
-      expect(@location.name).to eq("new york, ny")
+      expect(location.name).to eq("new york, ny")
     end
   end
 
