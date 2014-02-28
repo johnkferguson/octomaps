cc78cf48c2c3d2e88f5300972eb67ff868fcd5fe
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-02-28 11:01:40 -0500

    Set up Rails Neo4j template app

---
+++ octomaps/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Octomaps::Application.config.secret_key_base = '9ffea52d3051bd1e0a3f7116e3246db742d3fa6e599134ddde07daddf90cb0e686ce7e4086a5d55ffe16b98275eb4f8b214b3993a060020616ad40e01afeb9d0'
