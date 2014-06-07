3777574d11946dada78840586df462ede8d8b239
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-07 19:00:52 -0400

    Define all Repository properties

--- octomaps-rails/app/models/repository.rb
+++ octomaps-rails/app/models/repository.rb
@@ -3,16 +3,23 @@ class Repository
 
   property :created_at
   property :updated_at
-  property :owner, type: String
-  property :name, type: String
   property :full_name, type: String
+  property :github_id, type: Integer
+  property :name, type: String
   property :description, type: String
-  property :forks_count, type: Integer
-  property :stargazers_count, type: Integer
-  property :watchers_count, type: Integer
+  property :homepage, type: String
+  property :size, type: Integer
   property :forked, type: Boolean
+  property :github_forks_count, type: Integer #github_forks_count
+  property :github_stargazers_count, type: Integer # stargazers_count
+  property :github_watchers_count, type: Integer # watchers_count
+  property :github_subscribers_count, type: Integer # subscribers_count
+  property :github_created_at, type: DateTime
+  property :github_updated_at, type: DateTime
+  property :github_pushed_at, type: DateTime
 
   has_n(:contributors).from(Person, :contributed_to)
+  # has_one(:creator).from(Person, :created_repository)
 end
 
 # repo2.contributors.create(joe, commits: 11)
--- octomaps-rails/spec/factories/repositories.rb
+++ octomaps-rails/spec/factories/repositories.rb
@@ -1,12 +1,18 @@
 FactoryGirl.define do
   factory :repository do
-    owner { "#{Faker::Internet.user_name}" }
+    full_name { "#{Faker::Name.first_name}/#{name}" }
+    github_id { Random.new.rand(1..100) }
     name { "#{Faker::Lorem.word}" }
-    full_name { "#{owner}/#{name}" }
     description { Faker::Company.catch_phrase }
-    forks_count { Random.new.rand(0..100) }
-    stargazers_count { Random.new.rand(0..100) }
-    watchers_count { Random.new.rand(0..100) }
+    homepage { Faker::Internet.http_url }
+    size { Random.new.rand(0..10000) }
     forked false
+    github_forks_count { Random.new.rand(0..100) }
+    github_stargazers_count { Random.new.rand(0..100) }
+    github_watchers_count { Random.new.rand(0..100) }
+    github_subscribers_count { Random.new.rand(0..100) }
+    github_created_at DateTime.new
+    github_updated_at DateTime.new
+    github_pushed_at DateTime.new
   end
 end
