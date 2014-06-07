ba05d7732149758e4059e8a3bae1585917fe6039
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-07 00:02:20 -0400

    Configure Octokit to use Faraday HTTP cache

---
+++ octomaps-rails/config/initializers/octokit_middleware.rb
@@ -0,0 +1,7 @@
+stack = Faraday::RackBuilder.new do |builder|
+  builder.use Faraday::HttpCache
+  builder.use Octokit::Response::RaiseError
+  builder.adapter Faraday.default_adapter
+end
+
+Octokit.middleware = stack
