5caed3734c3a3d904c0360efb3e6f44d2ef8a333
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-07 18:18:31 -0400

    Namespace Github Services under Github module

---
+++ octomaps-rails/app/services/github/connection.rb
@@ -0,0 +1,15 @@
+module Github
+  class Connection
+    def client
+      @client ||= begin
+        config_file = File.expand_path(File.join(__FILE__, "../../../../config/github.yml"))
+        config ||= YAML.load(ERB.new(File.read(config_file)).result)
+        Octokit::Client.new(client_id: config['client_id'], client_secret: config['client_secret'])
+      end
+    end
+
+    def rate_limit_remaining
+      client.rate_limit.remaining
+    end
+  end
+end
---
+++ octomaps-rails/app/services/github/user.rb
@@ -0,0 +1,34 @@
+module Github
+  class User < Connection
+    attr_reader :username
+
+    def initialize(username)
+      @username = username
+    end
+
+    def attributes
+      @attributes ||= client.user(username).to_h
+    end
+
+    [
+      :login,
+      :id,
+      :name,
+      :email,
+      :avatar_url,
+      :gravatar_id,
+      :company,
+      :blog,
+      :hireable,
+      :bio,
+      :public_repos,
+      :public_gists,
+      :followers,
+      :following,
+      :created_at,
+      :updated_at
+    ].each do |key|
+      define_method(key) { attributes.fetch(key) }
+    end
+  end
+end
