e11c9f0db7f5d8bcfc683c203290e801f3befacb
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-08 13:54:13 -0400

    Clean up VCR configuration

--- rails/spec/support/vcr.rb
+++ rails/spec/support/vcr.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
-  config.around(:each, :vcr) do |example|
-    name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
-    VCR.use_cassette(name) { example.call }
+  config.around(:each, :vcr) do |spec|
+    paths_and_filename = paths_and_filename(spec.metadata[:full_description])
+    VCR.use_cassette(paths_and_filename) { spec.call }
   end
 end
 
@@ -10,4 +10,8 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.default_cassette_options = { record: :new_episodes }
+end
+
+def paths_and_filename(description)
+  description.split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
 end
