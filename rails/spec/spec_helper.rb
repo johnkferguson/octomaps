14c2b759d4425d1437118e94cd1829eee4216995
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-08 17:47:10 -0400

    Reorganize spec/support

--- rails/spec/spec_helper.rb
+++ rails/spec/spec_helper.rb
@@ -1,24 +1,15 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'vcr'
 require 'webmock/rspec'
 require 'rspec/its'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.order = "random"
+  config.order = 'random'
   config.include FactoryGirl::Syntax::Methods
-  config.include Neo4jDatabaseHelpers
   config.infer_spec_type_from_file_location!
-
-  config.before(:all) do
-    prepare_database
-  end
-
-  config.before(:each) do
-    prepare_database unless current_session
-  end
 end
---
+++ rails/spec/support/config/neo4j.rb
@@ -0,0 +1,37 @@
+RSpec.configure do |config|
+  config.before(:all) do
+    prepare_database
+  end
+
+  config.before(:each) do
+    prepare_database unless current_session
+  end
+
+  config.after(:each, :neo4j) do
+    delete_database
+  end
+end
+
+def prepare_database
+  close_session
+  start_server
+  delete_database
+end
+
+def current_session
+  Neo4j::Session.current
+end
+
+def delete_database
+  Neo4j::Session.current._query('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
+end
+
+private
+
+def close_session
+  current_session.close if current_session
+end
+
+def start_server
+  Neo4j::Session.open(:server_db, 'http://localhost:7474')
+end
