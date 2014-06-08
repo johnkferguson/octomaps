dbfaf763029056774f79bb4e5fa21b178a16dd2a
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-08 11:50:21 -0400

    Remove unnecessary comments in models

--- rails/app/models/person.rb
+++ rails/app/models/person.rb
@@ -3,7 +3,7 @@ class Person
 
   property :created_at
   property :updated_at
-  property :github_username, type: String # username
+  property :github_username, type: String
   property :github_id, type: Integer
   property :name, type: String
   property :email, type: String
@@ -13,10 +13,10 @@ class Person
   property :blog, type: String
   property :hireable, type: Boolean
   property :bio, type: String
-  property :github_public_repos_count, type: Integer # public_repos
-  property :github_public_gists_count, type: Integer # public_gists
-  property :github_followers_count, type: Integer # followers
-  property :github_following_count, type: Integer # following
+  property :github_public_repos_count, type: Integer
+  property :github_public_gists_count, type: Integer
+  property :github_followers_count, type: Integer
+  property :github_following_count, type: Integer
   property :github_created_at, type: DateTime
   property :github_updated_at, type: DateTime
 
--- rails/app/models/repository.rb
+++ rails/app/models/repository.rb
@@ -10,10 +10,10 @@ class Repository
   property :homepage, type: String
   property :size, type: Integer
   property :forked, type: Boolean
-  property :github_forks_count, type: Integer # forks_count
-  property :github_stargazers_count, type: Integer # stargazers_count
-  property :github_watchers_count, type: Integer # watchers_count
-  property :github_subscribers_count, type: Integer # subscribers_count
+  property :github_forks_count, type: Integer
+  property :github_stargazers_count, type: Integer
+  property :github_watchers_count, type: Integer
+  property :github_subscribers_count, type: Integer
   property :github_created_at, type: DateTime
   property :github_updated_at, type: DateTime
   property :github_pushed_at, type: DateTime
