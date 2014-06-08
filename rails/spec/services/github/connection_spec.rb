0cfc06ee53b821dd1ffd5481867ea446ba372ec3
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-08 13:47:46 -0400

    Write specs for Github::Connection

---
+++ rails/spec/services/github/connection_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Github::Connection do
+  let(:connection) { described_class.new }
+
+  describe '#client' do
+    it 'is an instance of Octokit::Client' do
+      expect(connection.client).to be_a(Octokit::Client)
+    end
+  end
+
+  describe '#rate_limit_remaining', :vcr do
+    it 'returns an integer' do
+      expect(connection.rate_limit_remaining).to be_a(Integer)
+    end
+  end
+end
