282b768d663fb06ea8e3ae902589eb1a67749bd9
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-08 17:27:43 -0400

    Add rspec-its gem

--- rails/spec/spec_helper.rb
+++ rails/spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'vcr'
 require 'webmock/rspec'
+require 'rspec/its'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
