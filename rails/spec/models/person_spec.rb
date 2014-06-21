9b0b69a50432cf6644e584b566ed8dfb812a40c5
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-21 11:16:27 -0400

    Set up general factories spec to test all factories

--- rails/spec/models/person_spec.rb
+++ rails/spec/models/person_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Person do
-  it 'has a valid factory' do
-    expect(build(:person)).to be_valid
-  end
-
   describe 'validations' do
     let(:original) { create(:person) }
 
@@ -29,8 +25,8 @@ describe Person do
 
   describe '.persisted_usernames_from(usernames_array)', :neo4j do
     it 'returns only the usernames that have already been persisted' do
-      joe = create(:person, github_username: 'joe')
-      carl = create(:person, github_username: 'carl')
+      create(:person, github_username: 'joe')
+      create(:person, github_username: 'carl')
 
       expect(Person.persisted_usernames_from(usernames_array: ['joe', 'carl', 'john']))
         .to contain_exactly('joe', 'carl')
--- rails/spec/models/repository_spec.rb
+++ rails/spec/models/repository_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Repository do
-  it 'has a valid factory' do
-    expect(build(:repository)).to be_valid
-  end
-
   describe 'validations' do
     let(:original) { create(:repository) }
 
