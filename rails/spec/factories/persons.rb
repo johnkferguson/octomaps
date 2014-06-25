13359280f907a92a0dd96441f8f44fbdc05a19cf
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-06-25 14:52:35 -0400

    Remove bio from person factory

--- rails/spec/factories/persons.rb
+++ rails/spec/factories/persons.rb
@@ -7,7 +7,6 @@ FactoryGirl.define do
     avatar_url { Faker::Internet.http_url }
     gravatar_id { Faker::Lorem.characters(20) }
     company { Faker::Company.name }
-    blog { Faker::Internet.http_url }
     hireable false
     bio { Faker::Lorem.paragraph(3) }
     github_public_repos_count { Random.new.rand(0..100) }
